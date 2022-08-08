03e3b3f8ae6cce372955a4e5b167a11e75f79065
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-08-08 20:16:56 +0000

    Deploying to gh-pages from @ zablockiy/team-project-demo@201919c50a8a5a6fe8b74d57341ea684be9e7178 🚀

---
+++ index.1af1d6b5.js
@@ -0,0 +1,2 @@
+function scrrollToTop(){window.scrollTo({top:0,behavior:"smooth"})}window.addEventListener("scroll",(function(){document.querySelector(".scroll-top").classList.toggle("active",window.scrollY>500)}));
+//# sourceMappingURL=index.1af1d6b5.js.map
