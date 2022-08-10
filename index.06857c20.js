5ba63437faacbfbd5368e557916053892d42a9f1
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-08-10 16:36:52 +0000

    Deploying to gh-pages from @ zablockiy/team-project-demo@d72e9490295c254aaabb31dad9c4c4ddafc86f74 🚀

--- index.06857c20.js
+++
@@ -1,2 +0,0 @@
-$(".fade").slick({dots:!0,arrows:!1,infinite:!0,speed:500,fade:!0,cssEase:"linear"});
-//# sourceMappingURL=index.06857c20.js.map
---
+++ index.80c22fd6.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$(".customer-list").slick({arrows:!1,dots:!0,speed:2500,easing:"ease",autoplay:!0,autoplayspeed:6e3})}));
+//# sourceMappingURL=index.80c22fd6.js.map
