fceb5137bd86c3eea8a7648ccd098d4a3f69f50f
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-08-11 16:38:47 +0000

    Deploying to gh-pages from @ zablockiy/team-project-demo@39081ffd8a4e480fd58dca44c2b05072d36c0c88 🚀

---
+++ index.55db3345.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open1]"),closeModalBtn:document.querySelector("[data-modal-close1]"),modal:document.querySelector("[data-modal1]")};function o(){e.modal.classList.toggle("hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.55db3345.js.map
