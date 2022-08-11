4992e9f880c11ffad8f810c86f2a61fa3e229628
StanyslavCherniakov <stas.chernykov@gmail.com>
2022-08-11 22:15:18 +0000

    Deploying to gh-pages from @ zablockiy/team-project-demo@010fe8c3ad140a229aa4ed075614b6b7ebbc35ac 🚀

---
+++ index.2d9e0b37.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open3]"),closeModalBtn:document.querySelector("[data-modal-close3]"),modal:document.querySelector("[data-modal3]")};function o(){e.modal.classList.toggle("hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.2d9e0b37.js.map
