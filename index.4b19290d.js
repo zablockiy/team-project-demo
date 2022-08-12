21d30f32a3d8771d985632adf6463a1783dde929
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-08-12 06:01:19 +0000

    Deploying to gh-pages from @ zablockiy/team-project-demo@3d8219ef1b4a663e88b3613344edac0195cd7754 🚀

---
+++ index.4b19290d.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-open-loc]"),closeModalBtn:document.querySelector("[data-modal-close-loc]"),modal:document.querySelector("[data-modal-loc]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.4b19290d.js.map
