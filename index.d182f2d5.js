4933f717e72a69edc4bd19d7857cde893e7daebd
zablockiy <105744371+zablockiy@users.noreply.github.com>
2022-08-12 08:16:46 +0000

    Deploying to gh-pages from @ zablockiy/team-project-demo@3f06b02e83d3efdda8f8eb6870e1cec0afdcf7f4 🚀

---
+++ index.d182f2d5.js
@@ -0,0 +1,2 @@
+AOS.init({disable:!1,startEvent:"DOMContentLoaded",initClassName:"aos-init",animatedClassName:"aos-animate",useClassNames:!1,disableMutationObserver:!1,debounceDelay:50,throttleDelay:105,offset:100,delay:0,duration:500,easing:"ease",once:!1,mirror:!1,anchorPlacement:"top-bottom"});
+//# sourceMappingURL=index.d182f2d5.js.map
--- index.f21003f6.js
+++
@@ -1,2 +0,0 @@
-AOS.init({disable:!1,startEvent:"DOMContentLoaded",initClassName:"aos-init",animatedClassName:"aos-animate",useClassNames:!1,disableMutationObserver:!1,debounceDelay:50,throttleDelay:99,offset:100,delay:0,duration:900,easing:"ease",once:!1,mirror:!1,anchorPlacement:"top-bottom"});
-//# sourceMappingURL=index.f21003f6.js.map
